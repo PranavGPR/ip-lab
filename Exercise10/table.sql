2b451921bad83baa36b24751eace1432d393ce3a
sank2000 <santhoshvelr@yahoo.com>
2021-07-31 22:45:34 +0530

    added table sql file  ✔

---
+++ Exercise10/table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
---
+++ Exercise9/PhpWithDB/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
